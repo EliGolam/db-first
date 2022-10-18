b315295f083f558a66b320aef47d3245224734b3
EliGolam <elistasibusiness@gmail.com>
2022-10-18 18:06:03 +0200

    DB types added

--- DB_cars/cars.php
+++ DB_cars/cars.php
@@ -2,34 +2,42 @@
 
   /* 
   DATABASE STRUCTURE
+  [
+    "car_ID" => Incremental numbers from 1 - 16777215 UNSIGNED,
+    "maker" => String with max lenght 25
+    "model" => String with max length 255,
+    "trim" => String with max length 100,
+    "year" => year
 
-  car_models {
-    car_ID: Primary_KEY UNIQUE 
-    maker
-    model
-    year
-    priceLow
-    priceHigh
-    mileage
-    driver-side: right / left
-    body-type: sedan / sports / etc
-    fuel
-    transmission; Manual / automatic
-    drive: front / rear / etc
-    shifter: paddle/ stick / etc
-    torque: 
-    max-speed
-    acceleration
-    seats
-    tags/categories
+    "type" => "sedan",
+    "tags" => "compact",
 
+    "fuel" => "petrol",
+    "tank_litres" => 56.4,
+    "fuel_efficiency_low" => 6,
+    "fuel_efficiency_high" => 9.2,
+    "motor_cm3" => 1597, 
+    "emissions_g_100km" => 144, 
 
-    registration
-    warranty-years
-    warranty-type
-    VIN number
-    condition: new / used
-    quality null / 0 / 10
-  }
+    "transmission" => "auto",
+    "drive" => "FWD",
+    "cylinders" => "4",
+    "horsepower" => 174,
+    "torque_Nm" => 152,
+    "acceleration_100m_s" => 11,
+    "top_speed_km_h" => 196,
+
+    "seats" => 5,
+    "cargo_space_l" => 519,
+
+    "length_m" => 4.648,
+    "width_m" => 1.799,
+    "height_m" => 1.416, 
+
+    "msrp_euro" => 21000,
+    "base_warranty_years" => 3
+  ]
+  
+  
   */
 ?>
