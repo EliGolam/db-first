98242112506a9d297ac8f7d8b842067d2d4fee56
EliGolam <elistasibusiness@gmail.com>
2022-10-18 15:05:13 +0200

    first draft of the database

---
+++ DB_cars/cars.php
@@ -0,0 +1,35 @@
+<?php
+
+  /* 
+  DATABASE STRUCTURE
+
+  car_models {
+    car_ID: Primary_KEY UNIQUE 
+    maker
+    model
+    year
+    priceLow
+    priceHigh
+    mileage
+    driver-side: right / left
+    body-type: sedan / sports / etc
+    fuel
+    transmission; Manual / automatic
+    drive: front / rear / etc
+    shifter: paddle/ stick / etc
+    torque: 
+    max-speed
+    acceleration
+    seats
+
+
+
+    registration
+    warranty-years
+    warranty-type
+    VIN number
+    condition: new / used
+    quality null / 0 / 10
+  }
+  */
+?>
